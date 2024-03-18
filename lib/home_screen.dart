13667cb6d6d95ea9a1e6e929fb986a182f5d2754
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-03-18 21:21:22 +0400

    second commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,6 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.yellow,
         foregroundColor: Colors.blue,
@@ -21,20 +23,51 @@ class HomeScreen extends StatelessWidget {
             child: Icon(Icons.verified),
           ),
           Padding(
-            padding: EdgeInsets.only(right: 12),
+            padding: EdgeInsets.only(right: 50),
             child: Icon(Icons.search),
           )
         ],
       ),
-      body: const Center(
-        child: Icon(
-          Icons.lock,
-          size: 80,
-          color: Colors.grey,
-        ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 12, top: 12, bottom: 12, right: 12),
+            child: Container(
+                // alignment: Alignment.center,
+                width: 150,
+                height: 150,
+                color: Colors.black,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.male_outlined,
+                      size: 80,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "Male",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                )),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 12, right: 12, bottom: 12, left: 12),
+            child: Container(
+              width: 150,
+              height: 150,
+              color: Colors.black,
+              
+            ),
+          )
+        ],
       ),
     );
   }
 }
 
-List<int> x = [1, 2, 3, 4, 5];
+// List x = <int>[1,2,4,];
